3c12515738eb70d7b1598e9105d3524ce6e8885f
Ole Sivert Aarhaug <ole.sivert@aarhaug.com>
2020-10-20 13:57:07 +0200

    Added oam_register file

---
+++ src/design/oam_registers.sv
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.10.2020 12:33:39
+// Design Name: 
+// Module Name: oam_registers
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module oam_registers(oam_register, object_enabled);
+parameter OAMObjects = 64;
+
+output logic [31:0] oam_register [ OAMObjects - 1 : 0];
+output wire [OAMObjects - 1 :0] object_enabled;
+
+/*
+OAM: 2 16-bit addresses 
+    8 bit spriteref
+    10 bit x pos
+    10 bit y pos
+    1 bit priority
+    1 bit x-flip
+    1 bit y-flip
+    1 bit enable
+*/
+
+
+foreach ( oam_register[i] ) begin
+    assign object_enabled[i] = (oam_register[i][31:31] == 1);
+end
+
+
+endmodule
