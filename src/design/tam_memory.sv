da9738a3b33386c6990bf945792cf30fbecff7e7
Tor Andre Haugdahl <thaugdahl@gmail.com>
2020-10-27 08:49:48 +0100

    feat(TAM, Spritemem, Tilemem): Rough implementation of TAM, Sprite memory and Tile memory

--- src/design/tam_memory.sv
+++ src/design/tam_memory.sv
@@ -1,15 +1,31 @@
 `timescale 1ns / 1ps
 
+/**
+ * Memory Module to contain TAM entries for tilemaps 
+ *
+ * One TAM-entry is 16-bits.
+ **/
 module tam_memory(
     input wire clk,              // Clock to drive the RAM module
-    input wire[9:0]  read_addr,  // 5 bits to address 64 locations
-    input wire[6:0]  write_addr, // 6 bits to address 128 locations
+    input wire[10:0]  read_addr,  // 11 bits to address up to 2048 locations
+    input wire[10:0]  write_addr, // 11 bits to address up to 2048 locations
     input wire[15:0] write_data,
     input wire  write_enable,
 
     output reg [15:0] read_data
-
 );
 
+(* RAM_STYLE="BLOCK" *)
+reg[15:0] TAM_RAM [1199:0];
+
+    always_ff @(posedge clk) begin
+        if(write_enable) begin
+            TAM_RAM[write_addr] <= write_data;
+        end
+    end
+
+    always_comb begin
+        read_data = { TAM_RAM[read_addr] };
+    end
 
 endmodule
---
+++ src/testbench/vram_tile_memory_tb.sv
@@ -0,0 +1,51 @@
+module vram_tile_memory_tb(
+
+);
+
+    logic clk;
+    logic[11:0] read_addr;
+    logic [255:0] read_data;
+
+    logic write_enable;
+    logic[14:0] write_addr;
+    logic [15:0] write_data;
+
+
+    vram_tile_memory TILE(
+        
+    .clk(clk),              // Clock to drive the RAM module
+    .read_addr(read_addr),  // 12 bits to address 2048 sprite lines
+    .write_addr(write_addr), // 15 bits to address 32768 pixel pairs
+    .write_data(write_data),
+    .write_enable(write_enable),
+
+    .read_data(read_data)
+    );
+    
+
+    initial begin
+        clk = 0;
+        write_addr = 0;
+        write_data = $urandom%255;
+        write_enable = 0;
+
+        read_addr = 0;
+
+        #50
+        write_enable = 1;
+
+        for(int i = 0; i < 32768; i++) begin
+            #40
+            write_addr += 1;
+            write_data = $urandom%255;
+        end
+
+        #20
+        write_enable = 0;
+    end
+
+    always #20 clk = ~clk;
+
+
+endmodule
+
