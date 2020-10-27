ee499a5a941aa39dcb25c68dbe19dd6f8754b21f
Fridtjof Eikanger <fridtjoe@stud.ntnu.no>
2020-10-27 16:18:31 +0100

    Reworked prepare_line. Small change in sprite_drawer.

--- src/design/prepare_line.sv
+++ src/design/prepare_line.sv
@@ -19,36 +19,6 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module prepare_line(clk, reset, oam_data, sx, sy, oam_addr, BufferArray, line_prepeared);
-    parameter maxObjectPerLine = 32, OAMMaxObjects = 256, OAM_ADDR_SIZE=6;
-    localparam Start = 0, LineCheck = 1, Increment = 2, End = 3;
-
-    input wire clk;
-    input wire reset;
-    input wire [31:0] oam_data;
-    input wire [9:0] sx;
-    input wire [9:0] sy; 
-    output wire [5:0] oam_addr;
-    output logic [maxObjectPerLine - 1 : 0][OAM_ADDR_SIZE:0] BufferArray;
-    output logic line_prepeared;
-
-    logic [7:0] BufferArrayIndex, BufferArrayIndexBuffer;
-    logic [OAM_ADDR_SIZE - 1:0] OAMIndex, OAMIndexBuffer;
-    logic [2:0] state;
-    logic changeIny;
-    wire  [9:0] SpriteLineIndex;
-    wire  [0:0] SpriteOnLine;
-    logic [9:0] lastY;
-    logic line_prepeared_buffer;
-
-    /*
-
-SpriteLineIndex = CurrentLine - OAM.y_pos
-SpriteOnLine = SpriteLineIndex >= 0 && SpriteLineIndex < 16
-
-    */
-
 /*
 OAM: 2 16-bit addresses 
     8 bit spriteref
@@ -64,57 +34,96 @@ OAM: 2 16-bit addresses
 BufferArray 2 dim array [index]
 0 bit enabled
 1-8 bit OAM position
-
 */
 
-    assign oam_addr = OAMIndex;
-    assign SpriteLineIndex = sy - oam_data[27:17];
-    assign SpriteOnLine = (SpriteLineIndex >= 0) && (SpriteLineIndex < 16);
+module prepare_line(clk, reset, oam_data, sx, sy, oam_addr, BufferArray, line_prepeared);
+    parameter maxObjectPerLine = 32, OAMMaxObjects = 256, OAM_ADDR_SIZE=6;
 
-    always_ff @ (posedge clk or posedge reset) begin
-        OAMIndex <= OAMIndexBuffer;
-        BufferArrayIndex <= BufferArrayIndexBuffer;
-        lastY <= sy;
-        line_prepeared <= line_prepeared_buffer;
-        if (reset || lastY != sy) begin
-            state <= LineCheck;
-            BufferArrayIndex <= 0;
-            BufferArrayIndexBuffer <= 0;
-            OAMIndex <= 0;
-            OAMIndexBuffer <= 0;
-            line_prepeared <= 0;
-            line_prepeared_buffer <= 0;
-            foreach ( BufferArray[i] ) begin
-                BufferArray[i] = 0;
+    input wire clk;
+    input wire reset;
+    input wire [31:0] oam_data;
+    input wire [9:0] sx;
+    input wire [9:0] sy; 
+    output wire [5:0] oam_addr;
+    output logic [maxObjectPerLine - 1 : 0][OAM_ADDR_SIZE:0] BufferArray;
+    output logic line_prepeared;
+
+    // Control signals
+    logic line_prepared_d, line_prepared_q;
+    logic last_sy, sy_changed;
+    logic should_write;
+
+    // Counters
+    int oam_index_d, oam_index_q, buffer_array_index_d, buffer_array_index_q;
+
+    // Object memory
+    logic [OAM_ADDR_SIZE-1:0] oam_index_d, oam_index_q;
+    logic [31:0] object;
+    int object_ypos;
+    assign object_ypos = object[27:18];
+
+    // Sprite calculations
+    int sprite_line;
+    logic sprite_is_on_line;
+
+    // Output array
+    logic [OAM_ADDR_SIZE:0] buffer_array_element_d;
+    logic [maxObjectPerLine-1:0][OAM_ADDR_SIZE:0] buffer_array_q;
+
+    // Wire assignments
+    assign oam_addr = oam_index_q;
+
+    always_comb begin
+        // Datapath
+        object = oam_data;
+
+        sprite_line = sy - object_ypos;
+        sprite_is_on_line = (sprite_line >= 0 && sprite_line < 16);
+
+        buffer_array_element_d <= {oam_addr, 1};
+        oam_index_d = oam_index_q + sprite_is_on_line;
+
+        // Counters
+        if (sy_changed) begin
+            oam_index_d = 0;
+            buffer_array_index_d = 0;
+        end else begin
+            if (line_prepared_d) begin
+                oam_index_d = oam_index_q;
+                buffer_array_index_d = buffer_array_index_q;
+            end else begin
+                oam_index_d = oam_index_q + 1;
+                buffer_array_index_d = buffer_array_index_q + 1;
             end
         end
-        else begin
-            case (state)
-            LineCheck:
-                if (OAMIndex == (OAMMaxObjects - 1)) begin
-                    state <= End;
-                end
-                else begin
-                    if (BufferArrayIndex < maxObjectPerLine && OAMIndex < (OAMMaxObjects - 1)  ) begin
-                        line_prepeared_buffer <= 0;
-                        if (oam_data[31:31] == 1 && SpriteOnLine == 1) begin
-                            BufferArray[BufferArrayIndex][OAM_ADDR_SIZE:1] <= OAMIndex;
-                            BufferArray[BufferArrayIndex][0:0] <= 1;
-                            BufferArrayIndexBuffer++;
-                        end
-                        OAMIndexBuffer++;
-                    end
-                    else begin
-                        state <= End;
-                    end
-                end
-            End:
-                begin
-                    state <= End;
-                    line_prepeared_buffer <= 1;
-                    OAMIndexBuffer <= 'bz;
-                end
-            endcase
+
+        // Control signals
+        sy_changed = last_sy != sy;
+        line_prepared_d = ((buffer_array_index_q >= maxObjectPerLine - 1) || (oam_index_q >= OAMMaxObjects - 1))  &&  ~sy_changed;
+        should_write = sprite_is_on_line && ~line_prepared_q;
+        
+        // Output
+        line_prepeared = line_prepared_q;
+        BufferArray = buffer_array_q;
+    end
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            oam_index_q <= 0;
+            buffer_array_index_q <= 0;
+            buffer_array_q <= 0;
+            last_sy <= 0;
+            line_prepared_q <= 0;
+        end else begin
+            oam_index_q <= oam_index_d;
+            buffer_array_index_q <= buffer_array_index_d;
+            if (should_write) begin
+                buffer_array_q[buffer_array_index_d] <= buffer_array_element_d;
+            end else if (sy_changed) begin
+                buffer_array_q <= 0;
+            end
+            last_sy <= sy;
+            line_prepared_q <= line_prepared_d;
         end
     end
 endmodule
